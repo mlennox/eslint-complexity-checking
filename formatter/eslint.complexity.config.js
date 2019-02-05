b2700cd8b2556a47f013a088ba32d7b3e887826a
Mark Lennox <mark@webpusher.ie>
2019-02-05 08:56:10 +0000

    fixed the formatter config, added error rules, suppressed node failure message

--- formatter/eslint.complexity.config.js
+++ formatter/eslint.complexity.config.js
@@ -1,35 +1,89 @@
-module.export = {
-  env: {
-    browser: true,
-    es6: true,
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
   },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 8,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      impliedStrict: true,
-      classes: true,
-    },
+  // "extends": ["eslint:recommended", "prettier"],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "impliedStrict": true,
+      "classes": true
+    }
   },
-  eslintIgnore: ['formatter/*.js'],
-  rules: {
-    'complexity': ['warn', 5],
-    'max-params': ['warn', 4],
-    'max-statements': ['warn', 7],
-    'max-statements-per-line': [
-      'warn',
-      {
-        max: 1,
-      },
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "complexity": [
+      "error",
+      5
     ],
-    'max-nested-callbacks': ['warn', 2],
-    'max-depth': [
-      'warn',
-      {
-        max: 2,
-      },
+    "max-params": ["warn", 4],
+    "max-statements": [
+      "warn",
+      7
     ],
-    'max-lines': ['warn', 80],
+    "max-statements-per-line": [
+      "warn",
+      {
+        "max": 1
+      }
+    ],
+    "max-nested-callbacks": [
+      "warn",
+      2
+    ],
+    "max-depth": [
+      "warn",
+      {
+        "max": 2
+      }
+    ],
+    "max-lines": [
+      "warn",
+      80
+    ],
   },
+
 };
+
+
+// module.export = {
+//   env: {
+//     browser: true,
+//     es6: true,
+//   },
+//   // extends: ["eslint:recommended", "prettier"],
+//   parser: 'babel-eslint',
+//   parserOptions: {
+//     ecmaVersion: 8,
+//     ecmaFeatures: {
+//       experimentalObjectRestSpread: true,
+//       impliedStrict: true,
+//       classes: true,
+//     },
+//   },
+//   eslintIgnore: ['formatter/*.js'],
+//   rules: {
+//     'complexity': ['error', 5],
+//     'max-params': ['error', 4],
+//     'max-statements': ['error', 7],
+//     'max-statements-per-line': [
+//       'warn',
+//       {
+//         max: 1,
+//       },
+//     ],
+//     'max-nested-callbacks': ['warn', 2],
+//     'max-depth': [
+//       'warn',
+//       {
+//         max: 2,
+//       },
+//     ],
+//     'max-lines': ['warn', 80],
+//   },
+// };
